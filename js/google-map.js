1180f1e8ced31e1dea4cdaeeffa3f441e52bb9b1
bpanda23 <bijaya.panda@sutramsolutions.com>
2026-06-09 18:22:43 +0530

    Google map integration

--- js/google-map.js
+++ js/google-map.js
@@ -5,13 +5,12 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
+    var myLatlng = new google.maps.LatLng(20.2278, 85.8583);
+    // Assotech World - Avenue 01, Rudrapur, Bhubaneswar, Odisha 751032
     
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 14,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
@@ -39,21 +38,29 @@ function init() {
     // Get the HTML DOM element that will contain your map 
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
+    if (!mapElement) {
+        return;
+    }
 
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['New York'];
+    var addresses = ['Assotech World - Avenue 01, Plot No. 274, adjacent to NH-16, Rudrapur, Bhubaneswar, Odisha 751032'];
 
     for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
+        $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address='+encodeURIComponent(addresses[x])+'&key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s', null, function (data) {
+            if (!data.results || !data.results[0]) {
+                return;
+            }
+            var p = data.results[0].geometry.location;
             var latlng = new google.maps.LatLng(p.lat, p.lng);
             new google.maps.Marker({
                 position: latlng,
                 map: map,
                 icon: 'images/loc.png'
             });
+            map.setCenter(latlng);
+            map.setZoom(15);
 
         });
     }
